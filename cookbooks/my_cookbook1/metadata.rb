5b828a27e5bcfa54ef82ece8331e345ac6da6ef8
Sampat Ponnaganti <sampat.p@gmail.com>
2015-11-01 04:12:44 +0000

    Added berks, gemfile

--- cookbooks/my_cookbook1/metadata.rb
+++ cookbooks/my_cookbook1/metadata.rb
@@ -1,7 +1,10 @@
 name             'my_cookbook1'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'sravz'
+maintainer_email 'sampat.p@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures my_cookbook1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends "chef-client"
+depends "apt"
+depends "ntp"
